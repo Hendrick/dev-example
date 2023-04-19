fd52e556ef4d207a4146055d5a3b0f4833debd29
geocodinglife <geocodinglife@gmail.com>
2023-04-19 17:35:42 -0500

    create files, update file and show working

--- app/controllers/blobs_controller.rb
+++ app/controllers/blobs_controller.rb
@@ -1,19 +1,47 @@
+# frozen_string_literal: true
+
 class BlobsController < ApplicationController
   include ActionController::HttpAuthentication::Basic::ControllerMethods
-  http_basic_authenticate_with name: Rails.configuration.file_api.basic_auth_username, password: Rails.configuration.file_api.basic_auth_password
+  http_basic_authenticate_with name: Rails.configuration.file_api.basic_auth_username,
+                               password: Rails.configuration.file_api.basic_auth_password
 
-  # POST /blobs/create
-  def create
-    json = params["blob"].as_json
-    uuid = SecureRandom.uuid
-    filename = uuid + ".json"
+  def index
+    file = File.read('received_files/data/description.json')
+    data = JSON.parse(file)
+    render json: data
+  end
 
-    if File.write("received_files/" + filename, JSON.pretty_generate(json))
-      json["document_id"] = uuid
-      json["document_filename"] = filename
-      render json: json, status: 201
+  def show
+    file_path = "received_files/#{params[:blob][:id]}_edited.json"
+    if File.exist?(file_path)
+      file_contents = File.read(file_path)
+      render json: JSON.parse(file_contents)
     else
-      render json: {error: "unprocessible_entry"}, status: 422
+      render json: { error: 'File not found' }, status: :not_found
+    end
+  end
+
+  def create
+    json = params['blob'].as_json
+    uuid = SecureRandom.uuid
+    filename = "#{uuid}.json"
+    filepath = "received_files/#{filename}"
+
+    if File.write(filepath, JSON.pretty_generate(json))
+      file_content = File.read(filepath)
+      updated_content = Blob.manipulate_file_content(file_content)
+      Blob.create_description_file(uuid, updated_content)
+
+      updated_filename = "#{uuid}_edited.json"
+      updated_filepath = "received_files/#{updated_filename}"
+
+      File.write(updated_filepath, JSON.pretty_generate(updated_content))
+      json['document_id'] = uuid
+      json['document_filename'] = filename
+      json['edited_filename'] = updated_filename
+      render json:, status: 201
+    else
+      render json: { error: 'unprocessible_entry' }, status: 422
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get "blobs/index", to: "blobs#index"
+  get '/blobs/:id', to: 'blobs#show'
 
   post "blobs/create", to: "blobs#create"
 end
